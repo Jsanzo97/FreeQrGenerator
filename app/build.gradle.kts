546e02b4c1b58e632cf0650000da455ca689e451
Jorge Sanzo Hernando <jorge.sanzo@smallworldfs.com>
2023-10-14 14:21:45 +0200

    Add ViewModel, improve state management and improve errors and visual issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("com.github.alexzhirkevich:custom-qr-generator:2.0.0-alpha01")
     implementation("com.google.accompanist:accompanist-drawablepainter:0.28.0")
     implementation("io.mhssn:colorpicker:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/freeqrgenerator/MainActivityStates.kt
+++ app/src/main/java/com/example/freeqrgenerator/MainActivityStates.kt
@@ -2,22 +2,16 @@ package com.example.freeqrgenerator
 
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
-import android.net.Uri
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import com.example.freeqrgenerator.ui.items.ColorSelector
 
-class MainState {
-
-    var url: String by mutableStateOf("")
-    var qrGenerated: Drawable? by mutableStateOf(null)
-    var imageUri: Uri? by mutableStateOf(null)
-    var bitmap: Bitmap? by mutableStateOf(null)
-    var showColorPicker: Boolean by mutableStateOf(false)
-    var foregroundColor: Color by mutableStateOf(Color.Black)
-    var backgroundColor: Color by mutableStateOf(Color.White)
-    var selectorMode: ColorSelector by mutableStateOf(ColorSelector.NONE)
-
-}
+data class MainState (
+    val url: String = "",
+    val qrGenerated: Drawable? = null,
+    val bitmap: Bitmap? = null,
+    val shouldShowColorPicker: Boolean = false,
+    val foregroundColor: Color = Color.Black,
+    val backgroundColor: Color = Color.White,
+    val selectorMode: ColorSelector = ColorSelector.NONE,
+    val error: MainError = MainError.NONE
+)
